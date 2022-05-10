a0e4249422b47008604d9fb07faa938d167d609c
JonasGN <j.goncalvesneto@outlook.com>
2022-05-10 18:59:38 -0300

    [frontend] add fast refresh config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+
 export function App() {
-  return <h1>Hello, world!</h1>;
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(++counter);
+  }
+
+  return (
+    <>
+      <h2>{counter}</h2>
+      <button type="button" onClick={increment}>
+        Increment +1
+      </button>
+    </>
+  );
 }
