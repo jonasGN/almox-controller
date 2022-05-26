8f84e6fad84d9bdca2f56f7ba6e95c269aa3ca56
JonasGN <j.goncalvesneto@outlook.com>
2022-05-25 22:41:16 -0300

    [frontend] add Home page

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+import { ItemCard } from "../../components/ItemCard";
+import { ItemDetailsCard } from "../../components/ItemDetailsCard";
+import { Item } from "../../@types/entities";
+import { api } from "../../services/api";
+
+import styles from "./styles.module.scss";
+
+export default function HomePage() {
+  const [items, setItems] = useState<Item[]>([]);
+  const [itemFocused, setItemFocused] = useState<Item>({} as Item);
+
+  useEffect(() => {
+    async function loadItems() {
+      const response = await api.get<Item[]>("items");
+      const data = response.data.map((item) => ({
+        ...item,
+        unitPrice: item.unitPrice / 100,
+      }));
+
+      setItems(data);
+    }
+
+    loadItems();
+  }, []);
+
+  function handleCardFocused(itemId: number) {
+    const itemFocused = items.find((item) => item.id === itemId);
+
+    if (!itemFocused) return;
+    setItemFocused(itemFocused);
+  }
+
+  const isItemFocused = Object.keys(itemFocused).length !== 0;
+  const focusedSectionClass = isItemFocused ? styles.shrink : "";
+
+  return (
+    <main className={styles.main}>
+      <section className={`${styles.itemsSection} ${focusedSectionClass}`}>
+        {items.map((item) => {
+          return (
+            <ItemCard
+              key={item.id}
+              item={item}
+              isFocused={item.id === itemFocused.id}
+              onClick={() => handleCardFocused(item.id)}
+            />
+          );
+        })}
+      </section>
+
+      <ItemDetailsCard
+        item={itemFocused}
+        isVisible={isItemFocused}
+        onRequestClose={() => setItemFocused({} as Item)}
+      />
+    </main>
+  );
+}
