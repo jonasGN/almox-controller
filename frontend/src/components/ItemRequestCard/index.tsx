90ba3aa0e00621328594dd9f22701564be943307
JonasGN <j.goncalvesneto@outlook.com>
2022-06-07 14:35:39 -0300

    [frontend] add ItemRequestCard component

---
+++ frontend/src/components/ItemRequestCard/index.tsx
@@ -0,0 +1,36 @@
+import { ItemRequest } from "../../@types/entities";
+
+import { UserHeaderTile } from "../UserHeaderTile";
+
+import styles from "./styles.module.scss";
+
+interface ItemRequestCardProps {
+  item: ItemRequest;
+  onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+  isFocused?: boolean;
+}
+
+export function ItemRequestCard(props: ItemRequestCardProps) {
+  const { user, requestedItem: request } = props.item;
+  const activeClass = props.isFocused ? styles.active : "";
+
+  return (
+    <a
+      href="#"
+      className={`${styles.itemRequestCard} ${activeClass}`}
+      onClick={props.onClick}
+    >
+      <UserHeaderTile
+        id={user.id}
+        name={user.name}
+        companyId={user.companyCode}
+        actionDate={request.moment}
+      />
+      <p>{request.message}</p>
+      <div className={styles.codeContainer}>
+        <span>Código solicitado</span>
+        <strong>{request.itemCode}</strong>
+      </div>
+    </a>
+  );
+}
