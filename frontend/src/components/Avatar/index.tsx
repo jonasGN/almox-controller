a151da4cab963113d54cd76401174f2bb99228b4
jonasGN <j.goncalvesneto@outlook.com>
2022-08-14 17:39:36 -0300

    [frontend] add Avatar component

---
+++ frontend/src/components/Avatar/index.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { ElementException } from "../../exceptions";
+import { initialsOf } from "../../utils/formatters";
+import { classNames } from "../../utils/styles-helper";
+
+import { RawButton } from "../Buttons";
+
+import styles from "./styles.module.scss";
+
+interface AvatarProps {
+  userName?: string;
+  userImage?: string;
+  size?: "default" | "small";
+  radius?: "circle" | "square";
+  onClick?: (e: React.MouseEvent) => void;
+}
+
+export const Avatar = (props: AvatarProps): JSX.Element => {
+  const { userImage, userName, size, radius, onClick } = props;
+
+  const [image, setImage] = useState(userImage ?? "");
+
+  if (!userImage && !userName) {
+    throw new ElementException("Must be given at least one of: userImage or userName");
+  }
+
+  const initials = userName && initialsOf(userName);
+
+  const content = image ? (
+    <img src={image} alt={userName} onError={() => setImage("")} />
+  ) : (
+    <span>{initials}</span>
+  );
+
+  const buttonClasses = classNames(styles.avatarContainer, styles.avatarButton);
+  if (onClick) {
+    return (
+      <RawButton customClassName={buttonClasses} data-size={size} data-radius={radius}>
+        {content}
+      </RawButton>
+    );
+  }
+
+  return (
+    <div className={styles.avatarContainer} data-size={size} data-radius={radius}>
+      {content}
+    </div>
+  );
+};
--- frontend/src/components/Buttons/index.ts
+++ frontend/src/components/Buttons/index.ts
@@ -1,5 +1,6 @@
 import { MainButton } from "./MainButton";
 import { TextButton } from "./TextButton";
 import { IconButton } from "./IconButton";
+import { RawButton } from "./RawButton";
 
-export { MainButton, TextButton, IconButton };
+export { MainButton, TextButton, IconButton, RawButton };
