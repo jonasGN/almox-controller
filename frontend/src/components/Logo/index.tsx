c9abe08c4c7320814020a9b3cfebd092709fd9eb
jonasGN <j.goncalvesneto@outlook.com>
2022-08-14 18:58:05 -0300

    [frontend] add Logo component

---
+++ frontend/src/components/Logo/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.scss";
+
+interface LogoProps {
+  type?: "horizontal" | "vertical";
+}
+
+const horizontal = "/images/logo-horizontal.svg";
+const vertical = "/images/logo-vertical.svg";
+
+export const Logo = (props: LogoProps): JSX.Element => {
+  const { type = "horizontal" } = props;
+
+  return (
+    <div className={styles.logoContainer}>
+      <img
+        src={type === "horizontal" ? horizontal : vertical}
+        alt="Almox Controller logo"
+      />
+    </div>
+  );
+};
