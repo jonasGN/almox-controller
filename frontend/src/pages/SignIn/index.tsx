1460e758763884ee023b2a36d406338da6449143
jonasGN <j.goncalvesneto@outlook.com>
2022-08-14 19:04:14 -0300

    [frontend] update SignIn page

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,11 +1,13 @@
 import { SignInForm } from "./SignInForm";
 import { ScreenContainer } from "../../components/Layout";
+import { Logo } from "../../components/Logo";
 
 import styles from "./styles.module.scss";
 
 export const SignInPage = (): JSX.Element => {
   return (
     <ScreenContainer size="small" className={styles.container} useInlinePadding>
+      <Logo type="vertical" />
       <h1>Login</h1>
 
       <SignInForm />
