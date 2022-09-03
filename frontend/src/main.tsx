7b509bf6178301354b68d8750245406635dad435
jonasGN <j.goncalvesneto@outlook.com>
2022-09-03 14:51:08 -0300

    [frontend] add noscript page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,10 @@ import { AuthProvider } from "./context/AuthProvider";
 const container = document.getElementById("root") as HTMLElement;
 const root = ReactDOM.createRoot(container!);
 
+// when javascript is disabled the `data-js` will be in the html element
+const htmlElement = document.querySelector("html");
+htmlElement?.removeAttribute("data-js");
+
 root.render(
   <React.StrictMode>
     <AuthProvider>
